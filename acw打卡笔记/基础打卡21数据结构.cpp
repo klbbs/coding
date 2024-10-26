ed848bd837419105ad197e2b54497774a79fa533
kendas <27>
2024-10-26 20:01:05 +0800

    数据结构实践开始，理论算法暂停

---
+++ acw打卡笔记/基础打卡21数据结构.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+//看了别人的题解，多练加理解才是真正掌握
+using namespace std;
+
+const int N = 100005;
+
+int n[N], ne[N], idx = 1;
+
+void add_k(int x, int k)
+{
+    n[idx] = x, ne[idx] = ne[k], ne[k] = idx++;
+}
+
+void rm(int k)
+{
+    ne[k] = ne[ne[k]];
+}
+
+
+int main()
+{
+    int e;cin>>e;
+    for(int i = 1; i <= e; i ++ )
+    {
+        char a;cin>>a;
+        if(a == 'H')
+        {
+            int x;cin>>x;
+            add_k(x, 0);
+        }
+        else if( a == 'I')
+        {
+            int k,x;cin>>k>>x;
+            add_k(x, k);
+        }
+        else
+        {
+            int k; cin>>k;
+            rm(k);
+        }
+    }
+    for(int i = ne[0]; i; i = ne[i])
+    {
+        cout << n[i] << ' ';
+    }
+
+    return 0;
+}
+
